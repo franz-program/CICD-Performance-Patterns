5831cafa6fb8bde98041ed1ca131da838becaa57
Francesco Urdih <francesco.urdih@gmail.com>
2025-07-22 16:28:41 +0200

    single test workflow

---
+++ app/src/test/java/test/Test1.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+
+  @Test
+  public void waitTime() throws InterruptedException {
+    Thread.sleep(10000);
+  }
+
+}
---
+++ app/src/test/java/test/Test2.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+public class Test2 {
+
+  @Test
+  public void waitTime2() throws InterruptedException {
+    Thread.sleep(10000);
+  }
+
+}
