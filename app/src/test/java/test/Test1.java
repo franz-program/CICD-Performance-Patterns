0fa34887696bcd085001198a17e646848756bea2
Francesco Urdih <francesco.urdih@gmail.com>
2025-07-23 10:10:56 +0200

    testing bazel on github

---
+++ app/src/test/java/test/Test1.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.Test;
+
+public class Test1 {
+
+  @Test
+  public void test() throws InterruptedException {
+    System.out.println("Change");
+    System.out.println("Print");
+    Thread.sleep(5000);
+  }
+
+
+
+}
---
+++ app/src/test/java/test/Test2.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+
+public class Test2 {
+
+  @Test
+  public void test2() throws InterruptedException {
+    Thread.sleep(5000);
+  }
+
+}
